ef6e5862dc2ef85f2feaa19746b46debfe1b63e0
suckerface <9060047+suckerface@users.noreply.github.com>
2020-08-25 21:54:33 -0700

    .vimrc updates, fixed install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,17 @@ git submodule update --init --recursive
 
 # Symlinks files from repo to home directories
 ln -is $CWD/.applemusic $HOME
-ls -is $CWD/.battery.sh $HOME
+ln -is $CWD/.battery.sh $HOME
 ln -is $CWD/.p10k.zsh $HOME
 ln -is $CWD/.tmux.conf $HOME
 ln -is $CWD/.vimrc $HOME
 ln -is $CWD/.zshrc $HOME
 
 # Copy/configure p10k
-cp -r $CWD/powerlevel10k $HOME/.oh-my-zsh/custom/themes/
+ln -s $CWD/powerlevel10k $HOME/.oh-my-zsh/custom/themes/
 
-#ln -is $CWD/.oh-my-zsh ~/.oh-my-zsh
+# Copy/configure vundle - has to be symlink for plugin install
+ln -s $CWD/bundle $HOME/.vim/
+
+# Install and configure Vundle options
+vim ~/.vimrc +PluginInstall +x +q!
